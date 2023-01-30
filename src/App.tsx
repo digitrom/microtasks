7112ca2794a12e1ebb45aa06a80f42ca9ffcf6b5
digitrom <roma.bobrovski@gmail.com>
2023-01-30 19:02:59 +0300

    Map method homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import Button from "./monday_super-button/Button";
-import {NewComponent} from "./map_method/NewComponent";
+import {NewComponent_1} from "./map_method/NewComponent";
 
 export type StudentType = {
     id: number
@@ -9,50 +9,60 @@ export type StudentType = {
     age: number
 }
 
+export type topCarType = {
+    manufacturer: string
+    model: string
+}
+
 
 function App() {
 
-    const students: Array<StudentType>  = [
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
+    // const students: Array<StudentType> = [
+    //     {id: 1, name: "James", age: 8},
+    //     {id: 2, name: "Robert", age: 18},
+    //     {id: 3, name: "John", age: 28},
+    //     {id: 4, name: "Michael", age: 38},
+    //     {id: 5, name: "William", age: 48},
+    //     {id: 6, name: "David", age: 58},
+    //     {id: 7, name: "Richard", age: 68},
+    //     {id: 8, name: "Joseph", age: 78},
+    //     {id: 9, name: "Thomas", age: 88},
+    //     {id: 10, name: "Charles", age: 98},
+    //     {id: 11, name: "Christopher", age: 100},
+    // ]
+
+    const topCars: Array<topCarType> = [
+        {manufacturer: "BMW", model: 'm5cs'},
+        {manufacturer: "Mercedes", model: 'e63s'},
+        {manufacturer: "Audi", model: 'rs6'}
     ]
 
 
+    // const func1 = (name:string, age: number) => {
+    //   console.log(name, age)
+    // }
 
-  // const func1 = (name:string, age: number) => {
-  //   console.log(name, age)
-  // }
+    // let [a, setA] = useState(0)
+    //
+    // const onClickHandler_1 = () => {
+    //     setA(++a)
+    //     console.log(a)
+    // }
+    //
+    //   const onClickHandler_2 = () => {
+    //       setA(0)
+    //       console.log(a)
+    //   }
 
-  // let [a, setA] = useState(0)
-  //
-  // const onClickHandler_1 = () => {
-  //     setA(++a)
-  //     console.log(a)
-  // }
-  //
-  //   const onClickHandler_2 = () => {
-  //       setA(0)
-  //       console.log(a)
-  //   }
-
-  return (
-      <div className="App">
-        {/*<Button name={"Roman"} callBack={() => func1("Roman", 37)}/>*/}
-        {/*<button onClick={onClickHandler_1}>{a}</button>*/}
-        {/*<button onClick={onClickHandler_2}>{"reset"}</button>*/}
-          <NewComponent students={students}/>
-
-      </div>
-  );
+    return (
+        <div className="App">
+            {/*<Button name={"Roman"} callBack={() => func1("Roman", 37)}/>*/}
+            {/*<button onClick={onClickHandler_1}>{a}</button>*/}
+            {/*<button onClick={onClickHandler_2}>{"reset"}</button>*/}
+            {/*  <NewComponent students={students}/>*/}
+            <NewComponent_1 topCars={topCars} />
+        </div>
+    );
 }
 
 export default App;
--- src/map_method/NewComponent.tsx
+++ src/map_method/NewComponent.tsx
@@ -1,23 +1,44 @@
 import React from "react";
-import {StudentType} from "../App";
+import {StudentType, topCarType} from "../App";
 
-type NewComponentPropsType = {
-    students: Array<StudentType>
+// type NewComponentPropsType = {
+//     students: Array<StudentType>
+// }
+type NewComponent_1Type = {
+    topCars: Array<topCarType>
 }
 
+// export const NewComponent = (props: NewComponentPropsType) => {
+//     return (
+//         <ul>
+//             {props.students.map((oneObjectFromStudentArray, index) => {
+//                 return (
+//                     <li key={oneObjectFromStudentArray.id}>
+//                         <span>{oneObjectFromStudentArray.name}</span>
+//                         <span> age: {oneObjectFromStudentArray.age}</span>
+//                     </li>
+//                 )
+//             })}
+//         </ul>
+//     )
 
-export const NewComponent = (props: NewComponentPropsType) => {
+export const NewComponent_1 = (props: NewComponent_1Type) => {
     return (
-        <ul>
-            {props.students.map((oneObjectFromStudentArray, index) => {
+        <table>
+            <tr>
+                <th>manufacturer</th>
+                <th>model</th>
+            </tr>
+            {props.topCars.map((carObjectFromTopCarsArray, index) => {
+                debugger
                 return (
-                    <li key={oneObjectFromStudentArray.id}>
-                        <span>{oneObjectFromStudentArray.name}</span>
-                        <span> age: {oneObjectFromStudentArray.age}</span>
-                    </li>
+                    <tr key={index}>
+                        <td>{index+1} {carObjectFromTopCarsArray.manufacturer}</td>
+                        <td>{carObjectFromTopCarsArray.model}</td>
+                    </tr>
                 )
             })}
-        </ul>
+        </table>
     )
 }
 
