5cea98d397b98b71ca238ec04e68a1be75d8eb36
digitrom <roma.bobrovski@gmail.com>
2023-02-14 00:38:15 +0300

    Input - part 2 - App.tsx finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {FullInput} from "./monday_input/components/FullInput";
+import {Button} from "./monday_input/components/Button";
+import {Input} from "./monday_input/components/Input";
 
 export type MessageType = {
     message: string
@@ -8,26 +10,35 @@ export type MessageType = {
 const App = () => {
 
     let [message, setMessage] = useState<Array<MessageType>>([
-            {message: 'message1' },
+            {message: 'message1'},
             {message: 'message2'},
             {message: 'message3'}
         ]
     )
+    let [title, setTitle] = useState<string>('')
+
 
     const addMessage = (title: string) => {
         let newMessage: MessageType = {message: title}
         setMessage([newMessage, ...message])
     }
 
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
     return (
         <div className={'App'}>
-            <FullInput
-                addMessage={addMessage}
+            {/*<FullInput addMessage={addMessage}/>*/}
+            <Input
+                title={title}
+                setTitle={setTitle}
             />
-            {/*<div>*/}
-            {/*    <input/>*/}
-            {/*    <button>+</button>*/}
-            {/*</div>*/}
+            <Button
+                callBack={callBackButtonHandler}
+                name={"+"}
+            />
             {message.map((el, index) => {
                 return (
                     <div key={index}>{el.message}</div>
--- src/monday_input/components/FullInput.tsx
+++ src/monday_input/components/FullInput.tsx
@@ -1,15 +1,14 @@
 import React, {ChangeEvent, useState} from 'react';
-import {MessageType} from "../../App";
 
 export type FullInputTPropsType = {
     addMessage: (title: string) => void
 }
 
 export const FullInput = (props: FullInputTPropsType) => {
-   let [title, setTitle] = useState<string>("")
+    let [title, setTitle] = useState<string>("")
     const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
     const onClickButtonHandler = () => {
-       props.addMessage(title);
+        props.addMessage(title)
         setTitle('')
     }
     return (
